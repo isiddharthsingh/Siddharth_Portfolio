865895a219b1378548bfbd75a9ec88615b5c11ea
Siddharth Singh <singhsiddharth0127@gmail.com>
2024-08-17 07:07:16 -0400

    update files

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Siddharth Singh</span>
+            Hi Everyone, I am <span className="purple">Siddharth Singh </span>
             from <span className="purple">New York, NY.</span>
             <br />
             I am currently pursuing a Master of Computer Science at New York University (NYU).
