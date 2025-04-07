0435529da2bf2de974fb6927c911cd8b7b7ca361
Siddharth Singh <singhsiddharth0127@gmail.com>
2025-04-07 05:57:54 -0400

    fix build errors

--- src/components/About/ExperienceCard.js
+++ src/components/About/ExperienceCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
-import { Col, Row } from "react-bootstrap";
 
 // Define styles for experience items
 const experienceItemStyle = {
