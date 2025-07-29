b047fdf3e9f1dd5f092e028e0245c97c0ef6ce81
Siddharth Singh <singhsiddharth0127@gmail.com>
2025-07-29 06:17:40 -0400

    restructure home layout with 3D model integration

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,46 +1,63 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
+import DesktopModel from "./DesktopModel";
 
 function Home() {
   return (
-    <section>
-      <Container fluid className="home-section" id="home">
+    <section style={{ margin: 0, padding: 0 }}>
+      {/* Particle background */}
+      <div
+        className="home-section"
+        id="home"
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      >
         <Particle />
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
+      </div>
 
-              <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> Siddharth Singh</strong>
-              </h1>
+      {/* Main content without extra top space */}
+      <section
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "2rem",
+          minHeight: "100vh",
+          marginTop: 0,
+        }}
+      >
+        {/* Text Content - 50% width */}
+        <section style={{ width: "60%", padding: "0 1rem" }}>
+          <h1 style={{ paddingBottom: 15 }} className="heading">
+            Hi There!{" "}
+            <span className="wave" role="img" aria-labelledby="wave">
+              👋🏻
+            </span>
+          </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
+          <h1 className="heading-name">
+            I'M<strong className="main-name"> Siddharth Singh</strong>
+          </h1>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </Container>
+          <div style={{ paddingTop: 30, textAlign: "center" }}>
+            <Type />
+          </div>
+        </section>
+
+        {/* Model - 50% width */}
+        <section style={{ width: "40%", padding: "0 1rem" }}>
+          <DesktopModel />
+        </section>
+      </section>
+
       <Home2 />
     </section>
   );
